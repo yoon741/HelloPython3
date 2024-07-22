df5119075ecbd0f67f79210cf7e509f39e57da1a
yoon741 <mono74140@gmail.com>
2024-07-22 10:52:24 +0900

    성적처리 프로그램 v7 - 데이터베이스 getTotalSungJuk

---
+++ db/sungjul-sqlite.sql
@@ -0,0 +1,40 @@
+drop table sungjuk;
+
+-- 성적 테이블 생성
+create table sungjuk (
+                         sjno    integer     primary key autoincrement,
+                         name    varchar(10) not null,
+                         kor     int         not null,
+                         eng     int         not null,
+                         mat     int         not null,
+                         total   int         not null,
+                         avg     decimal(5,1),
+                         grd     varchar(2),
+--  regdate datetime default (datetime('now'))  -- UTC(국제표준시로)
+                         regdate datetime default (datetime('now','localtime'))  -- UTC +9 (현재시에 맞춰서)
+);
+
+-- 성적 데이터 추가
+insert into sungjuk (name, kor, eng, mat, total, avg, grd)
+values ('abc123', 99,99,99,297, 99.0, '수');
+
+-- 성적 데이터 전체 조회
+select * from sungjuk;
+
+-- 성적 데이터 중 이름, 국어, 영어, 수학만 조회
+select name, kor, eng, mat from sungjuk;
+
+-- 성적 데이터 중 학생번호, 이름, 국어, 영어, 수학, 등록일만 조회
+select
+    sjno, name, kor, eng, mat, substr(regdate,0,11) regdate
+from sungjuk;
+
+-- 성적 데이터 중 이름으로 검색해서 모두 조회
+select * from sungjuk where name = 'abc123';
+
+-- 성적 데이터 중 학생번호로 검색해서 모두 조회
+select * from sungjuk where sjno = 1;
+
+-- 성적 데이터 총 갯수 조회
+select count(sjno) togal from sungjuk;
+-- count할때는 primarykey로 설정되어있는 칼럼으로
